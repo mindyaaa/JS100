9edee8a7c13635fcbba7b6e3a20730119f8314fe
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2023-03-28 15:55:31 +0900

    15번 문제 풀이

---
+++ 11-20/15.js
@@ -0,0 +1,11 @@
+// 신학기가 시작되고, 아이들이 돌아가면서 자기소개를 하기로 했습니다.
+
+// 만약 입력으로 김다정이라는 이름이 주어지면 "안녕하세요. 저는 김다정입니다."라고 출력하게 
+// 해주세요.
+
+let selfIntroduce = (myName) => {
+    return `안녕하세요. 저는 ${myName}입니다.`
+}
+
+let mindy = selfIntroduce('김민디');
+console.log(mindy);
